2c9b28475d10aeaf961b34d165ff5d9b3daa42c0
jahorta <jahorta00000@gmail.com>
2021-11-09 08:19:10 -0600

    Changed help viewer to a canvas with wordwrap and a scrollbar

--- SALSA/views.py
+++ SALSA/views.py
@@ -1273,9 +1273,19 @@ class ScriptView(tk.Frame):
 
 
 class HelpPopupView(tk.Toplevel):
-    offset = {
+    window_offset = {
         'x': 50,
         'y': 50
+    }
+
+    size = {
+        'width': 400,
+        'height': 400
+    }
+
+    text_offset = {
+        'x': 10,
+        'y': 10
     }
 
     def __init__(self, parent, title, about_text, position, callbacks, *args, **kwargs):
@@ -1283,15 +1293,22 @@ class HelpPopupView(tk.Toplevel):
 
         self.callbacks = callbacks
 
-        aboutLabel = tk.Label(self, text=about_text)
-        aboutLabel.grid(row=0, column=0)
+        help_canvas = tk.Canvas(self, width=self.size['width'], height=self.size['height'])
+        help_canvas.grid(row=0, column=0)
+        help_canvas.create_text((self.text_offset['x'], self.text_offset['y']),
+                                anchor=tk.NW, text=about_text,
+                                width=self.size['width']-self.text_offset['x'])
 
         self.quit = tk.Button(self, text='Cancel', command=self.callbacks['on_close'])
         self.quit.grid(row=1, column=0)
+        canvas_scrollbar = tk.Scrollbar(self, orient="vertical", command=help_canvas.yview)
+        help_canvas.configure(yscrollcommand=canvas_scrollbar.set)
+        help_canvas.config(scrollregion=help_canvas.bbox("all"))
+        canvas_scrollbar.grid(row=0, column=1, sticky=tk.N+tk.S)
 
-        self.title('About')
+        self.title(title)
         self.resizable(width=False, height=False)
-        posX = position['x'] + self.offset['x']
-        posY = position['y'] + self.offset['y']
+        posX = position['x'] + self.window_offset['x']
+        posY = position['y'] + self.window_offset['y']
         pos = '+{0}+{1}'.format(posX, posY)
         self.geometry(pos)
