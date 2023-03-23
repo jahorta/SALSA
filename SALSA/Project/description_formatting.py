d36760563c4d6b7649cf9244c629c01346718f57
jahorta <jahorta00000@gmail.com>
2023-03-22 19:57:21 -0500

    Added support for displaying the memory location of a variable in the description

--- SALSA/Project/description_formatting.py
+++ SALSA/Project/description_formatting.py
@@ -1,3 +1,4 @@
+import math
 from typing import List
 
 from SALSA.BaseInstructions.bi_container import BaseInst
@@ -165,13 +166,73 @@ def hex_desc_params(param1: str, form='>'):
     return result
 
 
-desc_code_funcs = {'add': add_desc_params,
-                   'sub': subtract_desc_params,
-                   'mul': multiply_desc_params,
-                   'hex': hex_desc_params}
+# ---------------------------------------------------------------------- #
+# Variable name and location conversion functions and relevant variables #
+# ---------------------------------------------------------------------- #
+
+var_locs = {
+    'Bit': '0x80310b3c',
+    'Byte': '0x80310a1c',
+    'Int': '0x8030e3e4',
+    'Float': '0x8030e514'
+}
+
+var_sizes = {
+    'Bit': 1/8,
+    'Byte': 1,
+    'Int': 4,
+    'Float': 4
+}
 
 
-func_param_nums = {'add': [2],
-                   'sub': [2],
-                   'mul': [2],
-                   'hex': [1, 2]}
+def replace_vars_with_locs(param1: str):
+    var_pieces = param1.split('Var: ')
+    if len(var_pieces) == 1:
+        return param1
+    result = ' '.join(var_pieces[0].split(' ')[:-1])
+    for i in range(len(var_pieces)):
+        if i == len(var_pieces)-1:
+            result += ' '.join(var_pieces[i].split(' ')[-1])
+            continue
+        successful = True
+        var_type = var_pieces[i].split(' ')[-1]
+        if var_type not in var_locs:
+            successful = False
+        var_ind = var_pieces[i+1].split(" ")[0]
+        if not var_ind.isnumeric():
+            successful = False
+        offset = var_sizes[var_type] * int(var_ind)
+        offset_int = math.floor(offset)
+        loc = hex(int(var_locs[var_type], 16) + offset_int)
+        var_loc_str = loc
+        remainder = offset - offset_int
+        if var_sizes[var_type] < 0:
+            exponent = round(remainder * 8)
+            var_loc_str += f' & {padded_hex(2**exponent, 2)}'
+        if not successful:
+            result += f'{var_pieces[i]}Var: '
+            result += var_pieces[i+1].split(" ")[0]
+        else:
+            result += ' '.join(var_pieces[i].split(' ')[1:-1])
+            result += f' {var_loc_str}'
+
+
+# ------------------------------------------------------------------ #
+# Dicts for description code functions and allowed parameter numbers #
+# ------------------------------------------------------------------ #
+
+desc_code_funcs = {
+    'add': add_desc_params,
+    'sub': subtract_desc_params,
+    'mul': multiply_desc_params,
+    'hex': hex_desc_params,
+    'loc': replace_vars_with_locs
+}
+
+desc_code_param_nums = {
+    'add': [2],
+    'sub': [2],
+    'mul': [2],
+    'hex': [1, 2],
+    'loc': [1]
+}
