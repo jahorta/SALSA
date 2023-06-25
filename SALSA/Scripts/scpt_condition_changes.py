22c98eae0b473053ba5c7502cd37ebfa25562d17
jahorta <jahorta00000@gmail.com>
2023-06-24 22:59:09 -0500

    modified scpt types to work better with generating conditions

--- SALSA/Scripts/scpt_condition_changes.py
+++ SALSA/Scripts/scpt_condition_changes.py
@@ -1,10 +1,10 @@
 scpt_log_changes = {
-    '(1)!=0 and (2)!=0': {
+    '(1) !=0 and (2) !=0': {
         'bool-bool': "(1) and (2)"
     }
 }
 
 scpt_types = {
-    '(1)!=0 and (2)!=0': 'bool',
-    '(1)==(2)': 'bool'
+    '(1) !=0 and (2) !=0': 'bool',
+    '(1) == (2)': 'bool'
 }
