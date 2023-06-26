841264475cfc6914554604999b47cfaccf790d8b
jahorta <jahorta00000@gmail.com>
2023-06-26 08:54:48 -0500

    << and >> are no longer removed, an option has been added to remove the header, and the placeholder header text is now interpreted from that value

--- SALSA/Common/script_string_utils.py
+++ SALSA/Common/script_string_utils.py
@@ -4,7 +4,8 @@ string_replace_list = [
     ('\\c', ''),
     ('\\n', '\n'),
     ('', ' '),
-    ('…', '...')
+    ('…', '...'),
+    ('　', '')
 ]
 
 
@@ -23,15 +24,23 @@ def visible_to_SAstr(string):
 
 
 def SAstr_to_head_and_body(string):
-    head_start = string.find('《') + 1
-    head_end = string.find('》')
-    head = SAstr_to_visible(string[head_start:head_end])
-    body_start = string.find(')') + 1
-    body = SAstr_to_visible(string[body_start:])
-    return head, body
+    head_start = string.find('(') + 1
+    head_end = string.find(')')
+    temp_head = string[head_start:head_end]
+    if len(temp_head) == 0:
+        no_head = True
+        head = temp_head
+    else:
+        no_head = False
+        head = SAstr_to_visible(temp_head)
+    body = SAstr_to_visible(string[head_end+1:])
+    return no_head, head, body
 
 
-def head_and_body_to_SAstr(head, body):
-    head = visible_to_SAstr(head)
+def head_and_body_to_SAstr(no_head, head, body):
+    if no_head:
+        head = ''
+    else:
+        head = '　' if head == '' else visible_to_SAstr(head)
     body = visible_to_SAstr(body)
-    return f'\\h(《{head}》){body}\\e'
+    return f'\\h({head}){body}\\e'
