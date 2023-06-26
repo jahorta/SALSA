02e5da41b43852447e21ed71ef7f26363d882fd2
jahorta <jahorta00000@gmail.com>
2023-06-26 07:26:55 -0500

    reorganized into dict comprehension

--- SALSA/GUI/ParamEditorPopups/param_editor_controller.py
+++ SALSA/GUI/ParamEditorPopups/param_editor_controller.py
@@ -240,11 +240,7 @@ class ParamEditController:
         for var in old_var_list:
             if var not in new_var_list:
                 remove_list.append(var)
-        changes = {}
-        if len(add_list) > 0:
-            changes['add'] = add_list
-        if len(remove_list) > 0:
-            changes['remove'] = remove_list
+        changes = {'add': add_list if len(add_list) > 0 else [], 'remove': remove_list if len(remove_list) > 0 else []}
         return None if len(changes) == 0 else changes
 
     def get_vars(self, param_value, cur_list=None):
