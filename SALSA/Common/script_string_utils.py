3dc994a5200eeb7104f5b566cc1a09dadd337cb5
jahorta <jahorta00000@gmail.com>
2023-06-28 10:51:49 -0500

    string utils now detects if a string has a header by the presence of a \\h. Returns None for head if there is no header

--- SALSA/Common/script_string_utils.py
+++ SALSA/Common/script_string_utils.py
@@ -24,23 +24,30 @@ def visible_to_SAstr(string):
 
 
 def SAstr_to_head_and_body(string):
-    head_start = string.find('(') + 1
-    head_end = string.find(')')
-    temp_head = string[head_start:head_end]
-    if len(temp_head) == 0:
-        no_head = True
-        head = temp_head
+    head_end = -1
+    no_head = False
+    if '\\h' in string:
+        head_start = string.find('(') + 1
+        head_end = string.find(')')
+        temp_head = string[head_start:head_end]
+        if len(temp_head) == 0:
+            no_head = True
+            head = temp_head
+        else:
+            no_head = False
+            head = SAstr_to_visible(temp_head)
     else:
-        no_head = False
-        head = SAstr_to_visible(temp_head)
+        head = None
     body = SAstr_to_visible(string[head_end+1:])
     return no_head, head, body
 
 
 def head_and_body_to_SAstr(no_head, head, body):
+    body = visible_to_SAstr(body)
     if no_head:
         head = ''
-    else:
+    elif head is not None:
         head = '　' if head == '' else visible_to_SAstr(head)
-    body = visible_to_SAstr(body)
+    else:
+        return f'{body}\\e'
     return f'\\h({head}){body}\\e'
