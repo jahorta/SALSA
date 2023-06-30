f4d033b8e74064645cc77bda01139e0045074999
jahorta <jahorta00000@gmail.com>
2023-06-29 21:36:09 -0500

    corrected constructin of the font

--- SALSA/GUI/fonts_used.py
+++ SALSA/GUI/fonts_used.py
@@ -1,7 +1,7 @@
 
 class SALSAFont:
     default_family = 'Helvetica'
-    default_size = 15
+    default_size = 10
     default_suffix = ''
     default_select_suffix = 'underline'
     font: str
@@ -14,7 +14,7 @@ class SALSAFont:
         self.default_family = family if family is not None else self.default_family
         self.default_size = size if size is not None else self.default_size
         self.default_suffix = suffix if suffix is not None else self.default_suffix
-        self.default_select_suffix = select_suffix if select_suffix is not None else self.default_suffix
+        self.default_select_suffix = select_suffix if select_suffix is not None else self.default_select_suffix
         self.font = f'{self.default_family} {self.default_size} {self.default_suffix}'
         self.hover_font = f'{self.default_family} {self.default_size} {self.default_select_suffix}'
 
