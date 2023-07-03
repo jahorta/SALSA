8fe761ad8af9dbd7faaa493be30326e329cc41c7
jahorta <jahorta00000@gmail.com>
2023-07-02 21:26:41 -0500

    corrected parameter since case id always has a suffix of ' (case)'

--- SALSA/GUI/ProjectEditor/project_editor_controller.py
+++ SALSA/GUI/ProjectEditor/project_editor_controller.py
@@ -425,7 +425,8 @@ class ProjectEditorController:
 
             case = self.trees['instruction'].item(sel_iid)['values'][0]
 
-        inst_uuid = self.project.add_inst(self.current['script'], self.current['section'], pre_rowdata, case=case, direction=direction)
+        inst_uuid = self.project.add_inst(self.current['script'], self.current['section'],
+                                          pre_rowdata, case=case.split(' ')[0], direction=direction)
         inst_trace = [self.current['script'], self.current['section'], inst_uuid]
         self.update_tree('instruction', self.project.get_tree(self.view.get_headers('instruction'),
                                                               self.current['script'], self.current['section']))
