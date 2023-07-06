10dc7dea7246e843de57bb564e619be7d6a41e90
jahorta <jahorta00000@gmail.com>
2023-07-05 21:27:50 -0500

    now passes whether to insert at index zero of a group or below it

--- SALSA/GUI/ProjectEditor/project_editor_controller.py
+++ SALSA/GUI/ProjectEditor/project_editor_controller.py
@@ -619,9 +619,9 @@ class ProjectEditorController:
     def add_callback(self, key: str, callback: callable):
         self.callbacks[key] = callback
 
-    def on_move_items(self, tree_key, sel_bounds, insert_after):
+    def on_move_items(self, tree_key, sel_bounds, insert_after, insert_in_group):
         section = self.current['section'] if tree_key == 'instruction' else None
-        self.project.move_items(sel_bounds, insert_after, self.current['script'], section)
+        self.project.move_items(sel_bounds, insert_after, insert_in_group, self.current['script'], section)
         if tree_key == 'section':
             self.trees['instruction'].clear_all_entries()
         self.refresh_tree(tree_key)
