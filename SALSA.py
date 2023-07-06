7b738cfb3f42186faad4a6976466da291062be0f
jahorta <jahorta00000@gmail.com>
2023-07-05 19:34:41 -0500

    added a fake appid

--- SALSA.py
+++ SALSA.py
@@ -8,7 +8,7 @@ os.chdir(path)
 # for changing the taskbar icon
 if os.name == 'nt':
     import ctypes
-    myappid = 'mycompany.myproduct.subproduct.version' # arbitrary string
+    myappid = 'Chichis.SALSA.TomatoFree.1b'  # arbitrary string
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 if __name__ == '__main__':
