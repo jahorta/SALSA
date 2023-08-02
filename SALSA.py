0a206f3ca347d76fbd93767720851db4880471a7
jahorta <jahorta00000@gmail.com>
2023-08-02 09:04:01 -0500

    turned off for threaded breakpoints

--- SALSA.py
+++ SALSA.py
@@ -5,11 +5,11 @@ path = os.path.dirname(__file__)
 os.chdir(path)
 
 
-# for changing the taskbar icon
-if os.name == 'nt':
-    import ctypes
-    myappid = 'Chichis.SALSA.TomatoFree.1b'  # arbitrary string
-    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+# # for changing the taskbar icon, turn off for breakpoint in other threads
+# if os.name == 'nt':
+#     import ctypes
+#     myappid = 'Chichis.SALSA.TomatoFree.1b'  # arbitrary string
+#     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 if __name__ == '__main__':
     from SALSA.application import Application
