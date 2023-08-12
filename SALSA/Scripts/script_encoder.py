93a59c0e9d360abaffa215deefb544e10d56b82c
jahorta <jahorta00000@gmail.com>
2023-08-12 08:19:57 -0500

    now initializes self._additions to an empty dict

--- SALSA/Scripts/script_encoder.py
+++ SALSA/Scripts/script_encoder.py
@@ -94,6 +94,7 @@ class SCTEncoder:
         # for decoder, encoder validation only
         self.validation = validation
         self._EU_validation = eu_validation
+        self._additions = {}
         if self.validation and endian == 'big':
             if '099' in script.name:
                 self.sct_body = bytearray(b'\x4F\x00\x00\x00\x00\x00\x00\x04\x00\x00\x66\x43\x1D\x00\x00\x00\x00\x00\x00'
